387dc042be47fbb9815d176c4073e6a5dd9b9d36
Aleksei Tinkov <boofle@outlook.com>
2023-10-08 15:21:26 +0300

    Реализованы загрузка каталога, индикатор загрузки, алерт ошибок загрузки, сортировка, переход на экран коллекции

--- FakeNFT/UI/Catalog/Collection/CollectionViewController.swift
+++ FakeNFT/UI/Catalog/Collection/CollectionViewController.swift
@@ -2,6 +2,17 @@ import UIKit
 
 final class CollectionViewController: UIViewController {
 
+    var collectionId: String
+
+    init(collectionId: String) {
+        self.collectionId = collectionId
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .nftWhite
@@ -10,6 +21,8 @@ final class CollectionViewController: UIViewController {
         backItem.title = nil
         backItem.tintColor = .nftBlack
         navigationController?.navigationBar.topItem?.backBarButtonItem = backItem
+
+        print(">>> \(collectionId)")
 
     }
 
