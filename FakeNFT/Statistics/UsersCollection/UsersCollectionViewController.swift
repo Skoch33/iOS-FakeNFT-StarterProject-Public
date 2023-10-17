74257c940342210dad7386305c822b14799817c2
Konstantin-vetro <konstantin.vetro@yandex.ru>
2023-10-18 03:14:42 +0500

    Сделал верстку экрана коллекции пользователя, добавил вью модель

--- FakeNFT/Statistics/UsersCollection/UsersCollectionViewController.swift
+++ FakeNFT/Statistics/UsersCollection/UsersCollectionViewController.swift
@@ -4,14 +4,46 @@
 //
 
 import UIKit
+import ProgressHUD
 
 final class UserCollectionViewController: UIViewController {
+    // MARK: - SubView
+    private lazy var collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.minimumInteritemSpacing = 10
+        layout.minimumLineSpacing = 8
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.register(
+            UsersCollectionCell.self,
+            forCellWithReuseIdentifier: UsersCollectionCell.identifier
+        )
+        collectionView.allowsMultipleSelection = false
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.backgroundColor = .nftWhite
+        return collectionView
+    }()
+    // MARK: - ViewModel
+    private var viewModel: UsersCollectionViewModelProtocol
+
+    init(viewModel: UsersCollectionViewModelProtocol = UsersCollectionViewModel()) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    // MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
         setupNavigationBar()
-        setupUI()
+        setupCollectionView()
+        bind()
+        viewModel.loadData()
     }
-    
+    // MARK: - Setup Views
     private func setupNavigationBar() {
         if let topItem = navigationController?.navigationBar.topItem {
             topItem.backBarButtonItem = UIBarButtonItem(title: "",
@@ -23,7 +55,63 @@ final class UserCollectionViewController: UIViewController {
         navigationController?.navigationBar.tintColor = .nftBlack
     }
     
-    private func setupUI() {
+    private func setupCollectionView() {
         view.backgroundColor = .nftWhite
+        view.addSubview(collectionView)
+        // MARK: - AutoLayouts
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
+            collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            collectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+        ])
+    }
+    // MARK: - bind
+    private func bind() {
+        viewModel.isDataLoading = { isLoading in
+            if isLoading {
+                ProgressHUD.show()
+            } else {
+                ProgressHUD.dismiss()
+            }
+        }
+
+        viewModel.dataChanged = { [weak self] in
+            self?.collectionView.reloadData()
+        }
+    }
+}
+// MARK: - UICollectionViewDataSource
+extension UserCollectionViewController: UICollectionViewDataSource {
+    func collectionView(
+        _ collectionView: UICollectionView,
+        numberOfItemsInSection section: Int
+    ) -> Int {
+        return viewModel.nftCount()
+    }
+
+    func collectionView(
+        _ collectionView: UICollectionView,
+        cellForItemAt indexPath: IndexPath
+    ) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(
+            withReuseIdentifier: UsersCollectionCell.identifier,
+            for: indexPath
+        ) as? UsersCollectionCell else { return UICollectionViewCell()}
+
+        let nft = viewModel.nfts[indexPath.row]
+        let rating = nft.rating
+        cell.configure(nft: nft, rating: rating)
+        return cell
+    }
+}
+// MARK: - UICollectionViewDelegateFlowLayout
+extension UserCollectionViewController: UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+    func collectionView(
+        _ collectionView: UICollectionView,
+        layout collectionViewLayout: UICollectionViewLayout,
+        sizeForItemAt indexPath: IndexPath
+    ) -> CGSize {
+        return CGSize(width:  108, height: 192)
     }
 }
