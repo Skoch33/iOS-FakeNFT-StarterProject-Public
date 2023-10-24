3ab23a19c8e0939551e3a2b0fe178e8d74dfe20c
Konstantin-vetro <konstantin.vetro@yandex.ru>
2023-10-25 02:35:46 +0500

    реализовал появление онбординга при первом запуске приложения

--- FakeNFT/AppDelegate.swift
+++ FakeNFT/AppDelegate.swift
@@ -3,13 +3,23 @@ import UIKit
 @UIApplicationMain
 final class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
+    private let userDefaults = UserDefaults.standard
 
     func application(
         _ application: UIApplication,
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
     ) -> Bool {
         window = UIWindow()
-        window?.rootViewController = TabBarController()
+
+        let isFirstLaunch = userDefaults.bool(forKey: "isFirstLaunch")
+
+        if isFirstLaunch {
+            window?.rootViewController = TabBarController()
+        } else {
+            window?.rootViewController = OnboardingViewController()
+            userDefaults.set(true, forKey: "isFirstLaunch")
+        }
+
         window?.makeKeyAndVisible()
         return true
     }
